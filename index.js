8ea6126f3a68880ad65d748305518c00d64da54b
Wilderanyl <wilderanyl@gmail.com>
2023-09-21 09:04:13 +0700

    remove box shadow

--- index.js
+++ index.js
@@ -25,110 +25,450 @@ const quiz = [
       },
     ],
   },
-  // {
-  //   no: "Q2",
-  //   question: "Which of the following is used to create web pages?",
-  //   answers: [
-  //     {
-  //       li: "A",
-  //       answer: "HTML",
-  //       correct: true,
-  //     },
-  //     {
-  //       li: "B",
-  //       answer: "C",
-  //       correct: false,
-  //     },
-  //     {
-  //       li: "C",
-  //       answer: "JVM",
-  //       correct: false,
-  //     },
-  //     {
-  //       li: "D",
-  //       answer: "DTD",
-  //       correct: false,
-  //     },
-  //   ],
-  // },
-  // {
-  //   no: "Q3",
-  //   question: "HTML is considered as ____ language.",
-  //   answers: [
-  //     {
-  //       li: "A",
-  //       answer: "Programming Language",
-  //       correct: false,
-  //     },
-  //     {
-  //       li: "B",
-  //       answer: "OOP Language",
-  //       correct: false,
-  //     },
-  //     {
-  //       li: "C",
-  //       answer: "High Level Language",
-  //       correct: false,
-  //     },
-  //     {
-  //       li: "D",
-  //       answer: "Markup Language",
-  //       correct: true,
-  //     },
-  //   ],
-  // },
-  // {
-  //   no: "Q4",
-  //   question: "HTML is a set of markup ____.",
-  //   answers: [
-  //     {
-  //       li: "A",
-  //       answer: "Attributes",
-  //       correct: false,
-  //     },
-  //     {
-  //       li: "B",
-  //       answer: "Tags",
-  //       correct: true,
-  //     },
-  //     {
-  //       li: "C",
-  //       answer: "Sets",
-  //       correct: false,
-  //     },
-  //     {
-  //       li: "D",
-  //       answer: "Groups",
-  //       correct: false,
-  //     },
-  //   ],
-  // },
-  // {
-  //   no: "Q5",
-  //   question: "HTML tags are used to describe document ____.",
-  //   answers: [
-  //     {
-  //       li: "A",
-  //       answer: "Deonition",
-  //       correct: false,
-  //     },
-  //     {
-  //       li: "B",
-  //       answer: "Language",
-  //       correct: false,
-  //     },
-  //     {
-  //       li: "C",
-  //       answer: "Content",
-  //       correct: true,
-  //     },
-  //     {
-  //       li: "D",
-  //       answer: "None of those",
-  //       correct: false,
-  //     },
-  //   ],
-  // },
+  {
+    no: "Q2",
+    question: "Which of the following is used to create web pages?",
+    answers: [
+      {
+        li: "A",
+        answer: "HTML",
+        correct: true,
+      },
+      {
+        li: "B",
+        answer: "C",
+        correct: false,
+      },
+      {
+        li: "C",
+        answer: "JVM",
+        correct: false,
+      },
+      {
+        li: "D",
+        answer: "DTD",
+        correct: false,
+      },
+    ],
+  },
+  {
+    no: "Q3",
+    question: "HTML is considered as ____ language.",
+    answers: [
+      {
+        li: "A",
+        answer: "Programming Language",
+        correct: false,
+      },
+      {
+        li: "B",
+        answer: "OOP Language",
+        correct: false,
+      },
+      {
+        li: "C",
+        answer: "High Level Language",
+        correct: false,
+      },
+      {
+        li: "D",
+        answer: "Markup Language",
+        correct: true,
+      },
+    ],
+  },
+  {
+    no: "Q4",
+    question: "HTML is a set of markup ____.",
+    answers: [
+      {
+        li: "A",
+        answer: "Attributes",
+        correct: false,
+      },
+      {
+        li: "B",
+        answer: "Tags",
+        correct: true,
+      },
+      {
+        li: "C",
+        answer: "Sets",
+        correct: false,
+      },
+      {
+        li: "D",
+        answer: "Groups",
+        correct: false,
+      },
+    ],
+  },
+  {
+    no: "Q5",
+    question: "HTML tags are used to describe document ____.",
+    answers: [
+      {
+        li: "A",
+        answer: "Deonition",
+        correct: false,
+      },
+      {
+        li: "B",
+        answer: "Language",
+        correct: false,
+      },
+      {
+        li: "C",
+        answer: "Content",
+        correct: true,
+      },
+      {
+        li: "D",
+        answer: "None of those",
+        correct: false,
+      },
+    ],
+  },
+  {
+    no: "Q6",
+    question: "HTML document can contain.",
+    answers: [
+      {
+        li: "A",
+        answer: "Attributes",
+        correct: false,
+      },
+      {
+        li: "B",
+        answer: "Tags",
+        correct: false,
+      },
+      {
+        li: "C",
+        answer: "Plain text",
+        correct: false,
+      },
+      {
+        li: "D",
+        answer: "All of these",
+        correct: true,
+      },
+    ],
+  },
+  {
+    no: "Q7",
+    question: "Page designed in HTML is called a ",
+    answers: [
+      {
+        li: "A",
+        answer: "Yellow Page",
+        correct: false,
+      },
+      {
+        li: "B",
+        answer: "Web Page",
+        correct: true,
+      },
+      {
+        li: "C",
+        answer: "Server Page",
+        correct: false,
+      },
+      {
+        li: "D",
+        answer: "Front Page",
+        correct: false,
+      },
+    ],
+  },
+  {
+    no: "Q8",
+    question:
+      "We can write HTML code using ____. Select appropriate option(s).",
+    answers: [
+      {
+        li: "A",
+        answer: "VCL Media",
+        correct: false,
+      },
+      {
+        li: "B",
+        answer: "Notepad++",
+        correct: true,
+      },
+      {
+        li: "C",
+        answer: "Microsoft PowerPoint",
+        correct: false,
+      },
+      {
+        li: "D",
+        answer: "None of these",
+        correct: false,
+      },
+    ],
+  },
+  {
+    no: "Q9",
+    question: "HTML document is saved using ____ extension.",
+    answers: [
+      {
+        li: "A",
+        answer: ".htl",
+        correct: false,
+      },
+      {
+        li: "B",
+        answer: ".html",
+        correct: true,
+      },
+      {
+        li: "C",
+        answer: ".hml",
+        correct: false,
+      },
+      {
+        li: "D",
+        answer: ".htnl",
+        correct: false,
+      },
+    ],
+  },
+  {
+    no: "Q10",
+    question: "The software that can read and render HTML document is",
+    answers: [
+      {
+        li: "A",
+        answer: "Server",
+        correct: false,
+      },
+      {
+        li: "B",
+        answer: "Compiler",
+        correct: false,
+      },
+      {
+        li: "C",
+        answer: "Interpreter",
+        correct: false,
+      },
+      {
+        li: "D",
+        answer: "Browser",
+        correct: true,
+      },
+    ],
+  },
+  {
+    no: "Q11",
+    question:
+      "PCs running Windows 3.x will have ____ extension for html pages.",
+    answers: [
+      {
+        li: "A",
+        answer: "htl",
+        correct: false,
+      },
+      {
+        li: "B",
+        answer: "html",
+        correct: false,
+      },
+      {
+        li: "C",
+        answer: "htm",
+        correct: true,
+      },
+      {
+        li: "D",
+        answer: "hml",
+        correct: false,
+      },
+    ],
+  },
+  {
+    no: "Q12",
+    question: "Which of the following is not an example of browser?",
+    answers: [
+      {
+        li: "A",
+        answer: "Netscape Navigator",
+        correct: false,
+      },
+      {
+        li: "B",
+        answer: "Microsoft Bing",
+        correct: true,
+      },
+      {
+        li: "C",
+        answer: "Mozilla Firefox",
+        correct: false,
+      },
+      {
+        li: "D",
+        answer: "Opera",
+        correct: false,
+      },
+    ],
+  },
+  {
+    no: "Q13",
+    question: "Who is the primary author of HTML?",
+    answers: [
+      {
+        li: "A",
+        answer: "Brendan Eich",
+        correct: false,
+      },
+      {
+        li: "B",
+        answer: "Tim Berners-lee",
+        correct: true,
+      },
+      {
+        li: "C",
+        answer: "Web programmer",
+        correct: false,
+      },
+      {
+        li: "D",
+        answer: "Google Inc",
+        correct: false,
+      },
+    ],
+  },
+  {
+    no: "Q14",
+    question: "HTML was orst proposed in year ____.",
+    answers: [
+      {
+        li: "A",
+        answer: "1'80",
+        correct: false,
+      },
+      {
+        li: "B",
+        answer: "1990",
+        correct: true,
+      },
+      {
+        li: "C",
+        answer: "1''5",
+        correct: false,
+      },
+      {
+        li: "D",
+        answer: "2000",
+        correct: false,
+      },
+    ],
+  },
+  {
+    no: "Q15",
+    question: "HTML tags are surrounded by ____ brackets.",
+    answers: [
+      {
+        li: "A",
+        answer: "Angle",
+        correct: true,
+      },
+      {
+        li: "B",
+        answer: "Square",
+        correct: false,
+      },
+      {
+        li: "C",
+        answer: "Rounded",
+        correct: false,
+      },
+      {
+        li: "D",
+        answer: "Curly",
+        correct: false,
+      },
+    ],
+  },
+  {
+    no: "Q16",
+    question: "opening tag of HTML is called ",
+    answers: [
+      {
+        li: "A",
+        answer: "Ending tag",
+        correct: false,
+      },
+      {
+        li: "B",
+        answer: "Starting  tag",
+        correct: true,
+      },
+      {
+        li: "C",
+        answer: "Closed tag",
+        correct: false,
+      },
+      {
+        li: "D",
+        answer: "Pair tags",
+        correct: false,
+      },
+    ],
+  },
+  {
+    no: "Q17",
+    question: "HTML document contain one root tag called ____.",
+    answers: [
+      {
+        li: "A",
+        answer: "Head",
+        correct: false,
+      },
+      {
+        li: "B",
+        answer: "Title",
+        correct: false,
+      },
+      {
+        li: "C",
+        answer: "Body",
+        correct: false,
+      },
+      {
+        li: "D",
+        answer: "HTML",
+        correct: true,
+      },
+    ],
+  },
+  {
+    no: "Q18",
+    question: "Basic fundamental block is called as ",
+    answers: [
+      {
+        li: "A",
+        answer: "HTML tag",
+        correct: true,
+      },
+      {
+        li: "B",
+        answer: "HTML body",
+        correct: false,
+      },
+      {
+        li: "C",
+        answer: "HTML Attribute",
+        correct: false,
+      },
+      {
+        li: "D",
+        answer: "HTML Element",
+        correct: false,
+      },
+    ],
+  },
 ];
 
 const playAgainButton = document.getElementById("play-again");
@@ -225,8 +565,7 @@ nextBtn.addEventListener("click", () => {
       initialScore = 0;
       initialIndex = 0;
 
-      console.log(nextBtn)
-
+      console.log(nextBtn);
     }
   } else {
     playNowFun();
